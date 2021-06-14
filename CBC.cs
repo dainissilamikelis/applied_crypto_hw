bcc2977d7871ca7a479bb0a884e9d8765d29e280
Dainis Silamikelis <36889117+dainissilamikelis@users.noreply.github.com>
2021-06-14 16:16:39 +0300

    initial
    initial

---
+++ CBC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace hw
+{
+    public class CBC
+    {
+        public static byte[] Encrypt(byte[] plain_text, byte[] key, byte[] iv)
+        {
+
+            return plain_text; 
+        }
+    }
+}
---
+++ helper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace hw
+{
+    public class helper
+    {
+        public helper()
+        {
+        }
+    }
+}
