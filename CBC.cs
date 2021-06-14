b590898974ac6fe5db0b96800aa27523c851c14b
Dainis Silamikelis <dainis.silamikelis@gmail.com>
2021-06-14 16:32:27 +0300

    test git

--- CBC.cs
+++ CBC.cs
@@ -5,7 +5,7 @@ namespace hw
     {
         public static byte[] Encrypt(byte[] plain_text, byte[] key, byte[] iv)
         {
-
+            // this is to test if git is working, yo ? how come, you are only pushing random stuff
             return plain_text; 
         }
     }
